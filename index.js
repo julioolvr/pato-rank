8c881cc1c925fbf442db8f21b38e8e6a9d11c713
Julio <julio.olvr@gmail.com>
2019-10-12 21:02:28 -0300

    Fake ranking

--- index.js
+++ index.js
@@ -6,5 +6,17 @@ const port = 3000;
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => res.render("index"));
+app.get("/ranks", async (req, res) => {
+  const ranking = await getRanking();
+  res.render("ranks", { ranking });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+async function getRanking() {
+  return [
+    { name: "PATO 1", score: 1650 },
+    { name: "PATO 2", score: 1570 },
+    { name: "PATO 3", score: 1450 }
+  ];
+}
