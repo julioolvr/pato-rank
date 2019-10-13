1419049e92fc1415024219a67d3efe3572904efb
Julio <julio.olvr@gmail.com>
2019-10-13 00:04:46 -0300

    Very raw structure that calculates rankings after matches

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const _ = require("ramda");
 
 const app = express();
 const port = 3000;
@@ -6,17 +7,66 @@ const port = 3000;
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => res.render("index"));
+
+const glicko = require("glicko2");
+const rankingSettings = {
+  tau: 0.5,
+  rating: 1500,
+  rd: 200,
+  vol: 0.06
+};
+const ranking = new glicko.Glicko2(rankingSettings);
+
 app.get("/ranks", async (req, res) => {
-  const ranking = await getRanking();
-  res.render("ranks", { ranking });
+  const players = await getPlayers();
+  const matches = await getMatches();
+
+  applyMatchesToRankings(ranking, players, matches);
+
+  const playersByRank = Object.keys(players)
+    .map(key => ({ id: key, glicko: players[key] }))
+    .sort((a, b) => (a.glicko.getRating() > b.glicko.getRating() ? -1 : 1));
+
+  console.log({ playersByRank });
+
+  res.render("ranks", { ranking: playersByRank, matches });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-async function getRanking() {
+const FAKE_RANKING = [
+  { id: "PATO 1", rating: 1650, rd: 200, vol: 0.06 },
+  { id: "PATO 2", rating: 1570, rd: 200, vol: 0.06 },
+  { id: "PATO 3", rating: 1450, rd: 200, vol: 0.06 },
+  { id: "PATO 4", rating: 1427, rd: 200, vol: 0.06 },
+  { id: "PATO 5", rating: 1312, rd: 200, vol: 0.06 }
+];
+
+async function getPlayers() {
+  return _.mergeAll(
+    FAKE_RANKING.map(player => ({
+      [player.id]: ranking.makePlayer(player.rating, player.rd, player.vol)
+    }))
+  );
+}
+
+async function getMatches() {
   return [
-    { name: "PATO 1", score: 1650 },
-    { name: "PATO 2", score: 1570 },
-    { name: "PATO 3", score: 1450 }
+    [["PATO 1"], ["PATO 2"], ["PATO 3"], ["PATO 4"]],
+    [["PATO 1"], ["PATO 2", "PATO 4"], ["PATO 3"]],
+    [["PATO 2"], ["PATO 4"], ["PATO 1"], ["PATO 5"]],
+    [["PATO 3"], ["PATO 1", "PATO 5"]]
   ];
+}
+
+function applyMatchesToRankings(ranking, players, matches) {
+  const races = matches.map(match =>
+    ranking.makeRace(
+      match.map(playersInPosition =>
+        playersInPosition.map(playerId => players[playerId])
+      )
+    )
+  );
+
+  ranking.updateRatings(races.flatMap(race => race.getMatches()));
 }
