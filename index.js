b71379bf41d11a40e31ab82f5888527edc3a9592
Julio <julio.olvr@gmail.com>
2019-10-13 00:28:13 -0300

    Linting

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.get("/ranks", async (req, res) => {
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
+// eslint-disable-next-line no-unused-vars
 async function getMatches() {
   return [
     [["PATO 1"], ["PATO 3"], ["PATO 4"]],
@@ -30,6 +31,7 @@ async function getMatches() {
   ];
 }
 
+// eslint-disable-next-line no-unused-vars
 function applyMatchesToRankings(ranking, players, matches) {
   const races = matches.map(match =>
     ranking.makeRace(
